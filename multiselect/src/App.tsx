b9388ec38ea0267f5f8d80786ae6035917c67726
Maryna Rekish <marinarekish@gmail.com>
2024-02-27 17:06:37 +0100

    add multiple project initial files

---
+++ multiselect/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Select, SelectOption } from "./Select";
+
+const options = [
+  { label: "first", value: 1 },
+  { label: "second", value: 2 },
+  { label: "third", value: 3 },
+];
+
+// const handleChange = () => {
+//   console.log("oops");
+// };
+
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
+  return (
+    <>
+      <Select multiple options={options} value={value1} onChange={(o) => setValue1(o)} />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+    </>
+  );
+}
+export default App;
---
+++ multiselect/src/css/README.md
@@ -0,0 +1,15 @@
+# to add module css
+
+## 1 - create types folder in scr
+
+## 2 - add file
+
+declarations.d.ts
+
+## 3 - We need to tell TypeScript that when we write "import styles from './styles.css' we mean to load a module
+
+=> (to look for a './styles.scss.d.ts')
+
+### declare module "\*.module.css";
+
+done :)
